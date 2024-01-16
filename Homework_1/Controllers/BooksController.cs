e1ccd98d43ee2b0139b409ae4c2ae49cdd3f774a
Adil Yerzhanov <adil.erzhanov1990@gmail.com>
2024-01-16 06:31:31 +0100

    определил контрллеры в отдельную папку, переписал первый контроллер

---
+++ Homework_1/Controllers/BooksController.cs
@@ -0,0 +1,22 @@
+﻿using Homework_1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Homework_1.Controllers
+{
+    [ApiController]
+    [Route("api/books")]
+    public class BooksController : ControllerBase
+    {
+        private static readonly List<Book> Books = new List<Book>
+    {
+        new Book { Id = 1, Name = "test1" },
+        new Book { Id = 2, Name = "test2" }
+    };
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Book>> GetBooks()
+        {
+            return Ok(Books);
+        }
+    }
+}
---
+++ Homework_1/Models/Books.cs
@@ -0,0 +1,9 @@
+﻿namespace Homework_1.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+}
