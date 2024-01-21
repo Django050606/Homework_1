a1aa805d49c63af652aab6a318fd14092d660876
Adil Yerzhanov <adil.erzhanov1990@gmail.com>
2024-01-21 01:58:24 +0100

    Created the relation with data, started the changes with endpoints to suit the database, Swagger

--- Homework_1/Controllers/BooksController.cs
+++ Homework_1/Controllers/BooksController.cs
@@ -1,31 +1,102 @@
-﻿using Homework_1.Models;
+﻿using Homework_1.Data;
+using Homework_1.Models;
 using Microsoft.AspNetCore.Mvc;
-using static System.Reflection.Metadata.BlobBuilder;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+
 
 namespace Homework_1.Controllers
 {
     [ApiController]
-    [Route("api/books")]
+    [Route("api/[controller]")]
     public class BooksController : ControllerBase
     {
-        private static readonly List<Book> Books = new List<Book>
-    {
-        new Book { Id = 1, Name = "test1" },
-        new Book { Id = 2, Name = "test2" }
-    };
+        private readonly BooksApiDbContext dbContext;
+
+        public BooksController(BooksApiDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
 
         [HttpGet]
         public ActionResult<IEnumerable<Book>> GetBooks()
         {
-            return Ok(Books);
+            return Ok(dbContext.Books.ToList());
         }
 
 
-        [HttpGet]
-        [Route("api/books")]
-        public ActionResult GetBookByName(string name)//нахождение конкретной книги по имени
+        [HttpPost]
+        public IActionResult PostBook(AddBookRequest addBookRequest)
         {
-            var book = Books.Find(b => b.Name == name);
+            if (addBookRequest == null)
+            {
+                return BadRequest("Invalid data. Book data is null.");
+            }
+
+            var book = new Book()
+            {
+                Id = Guid.NewGuid(),
+                Name = addBookRequest.Name,
+                
+            };
+
+            dbContext.Books.Add(book);
+            dbContext.SaveChanges();
+
+            return CreatedAtAction(nameof(GetBookById), new { id = book.Id }, book);
+        }
+
+
+
+
+
+
+        [HttpPut("{id}")]
+        public ActionResult PutBook(int id, [FromBody] Book updatedBook)
+        {
+            if (updatedBook == null)
+            {
+                return BadRequest("Invalid data. Book data is null.");
+            }
+
+            var existingBook = dbContext.Books.Find(id);
+
+            if (existingBook == null)
+            {
+                return NotFound();
+            }
+
+            existingBook.Name = updatedBook.Name;
+
+            dbContext.SaveChanges();
+
+            return Ok(existingBook);
+        }
+
+
+
+        [HttpDelete("{id}")]
+        public ActionResult DeleteBookById(int id)
+        {
+            var bookToRemove = dbContext.Books.Find(id);
+
+            if (bookToRemove != null)
+            {
+                dbContext.Books.Remove(bookToRemove);
+                dbContext.SaveChanges();
+                return Ok(bookToRemove);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Book> GetBookById(int id)
+        {
+            var book = dbContext.Books.Find(id);
 
             if (book != null)
             {
@@ -36,62 +107,5 @@ namespace Homework_1.Controllers
                 return NotFound();
             }
         }
-
-        [HttpDelete]
-        [Route("api/books/{id}")]
-        public ActionResult DeleteBook(int id)//удаление
-        {
-            var bookToRemove = Books.FirstOrDefault(b => b.Id == id);
-
-            if (bookToRemove != null)
-            {
-                Books.Remove(bookToRemove);
-                return Ok(bookToRemove); // Возвращаем удаленную книгу в теле ответа, если нужно
-            }
-            else
-            {
-                return NotFound(); // Возвращаем 404
-            }
-        }
-
-        [HttpPost]
-        [Route("api/books")]
-        public ActionResult PostBook([FromBody] Book newBook)
-        {
-            if (newBook == null)
-            {
-                return BadRequest("Invalid data. Book data is null.");
-            }
-
-            //нифига я умный, просто добавил Count и вуаля, новая айдишка пхахах
-            newBook.Id = Books.Count + 1;
-
-            Books.Add(newBook);
-
-            return Created($"/api/books/{newBook.Id}", newBook);//возвращаем 201
-        }
-
-        [HttpPut]
-        [Route("api/books/{id}")]
-        public ActionResult PutBook(int id, [FromBody] Book updatedBook)
-        {
-            if (updatedBook == null)
-            {
-                return BadRequest("Invalid data. Book data is null.");
-            }
-
-            var existingBook = Books.FirstOrDefault(b => b.Id == id);
-
-            if (existingBook == null)
-            {
-                return NotFound(); // Книга с указанным идентификатором не найдена
-            }
-
-            
-            existingBook.Name = updatedBook.Name; //можем добавить другие поля по необходимости которые можно изменить 
-
-            return Ok(existingBook); // Возвращаем обновленную книгу в теле ответа
-        }
-
     }
 }
--- Homework_1/Models/Books.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Homework_1.Models
-{
-    public class Book
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-    }
-
-}
